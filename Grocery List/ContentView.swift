8c96ee39e864e2dbc3d39416cba892521149441d
Isaac Urbina <7547113+isaacurbina@users.noreply.github.com>
2025-02-25 15:18:21 -0600

    45. 2. SwiftData Model (#2)

--- Grocery List/ContentView.swift
+++ Grocery List/ContentView.swift
@@ -6,19 +6,31 @@
 //
 
 import SwiftUI
+import SwiftData
 
 struct ContentView: View {
+	
+	@Environment(\.modelContext) private var modelContext
+	@Query private var items: [Item]
+	
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
-        }
-        .padding()
+		NavigationStack {
+			List {
+				ForEach(items) { item in
+					Text(item.title)
+				}
+			}
+			.navigationTitle("Grocery List")
+			.overlay {
+				if items.isEmpty {
+					ContentUnavailableView("Empty Cart", systemImage: "cart.circle", description: Text("Add some items to the shopping list."))
+				}
+			}
+		}
     }
 }
 
 #Preview {
     ContentView()
+		.modelContainer(for: Item.self, inMemory: true)
 }
