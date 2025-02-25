b7f8af6240d14f8e81c9fe25af50f7175bdec623
Isaac Urbina <7547113+isaacurbina@users.noreply.github.com>
2025-02-25 14:48:46 -0600

    first commit

---
+++ Grocery List/Grocery_ListApp.swift
@@ -0,0 +1,17 @@
+//
+//  Grocery_ListApp.swift
+//  Grocery List
+//
+//  Created by Isaac Urbina on 2/25/25.
+//
+
+import SwiftUI
+
+@main
+struct Grocery_ListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
