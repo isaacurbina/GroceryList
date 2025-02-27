031b1de6f5682e269e5a3e8069d5a53b91f26b4d
Isaac Urbina <7547113+isaacurbina@users.noreply.github.com>
2025-02-26 18:08:16 -0600

    48. 5. Saving SwiftData

--- Grocery List/ContentView.swift
+++ Grocery List/ContentView.swift
@@ -12,6 +12,8 @@ struct ContentView: View {
 	
 	@Environment(\.modelContext) private var modelContext
 	@Query private var items: [Item]
+	@State private var item: String = ""
+	@FocusState private var isFocused: Bool
 	
 	func addEssentialFoods() {
 		modelContext.insert(Item(title: "Bakery & Bread", isCompleted: false))
@@ -23,8 +25,10 @@ struct ContentView: View {
 	
     var body: some View {
 		NavigationStack {
+			
 			List {
 				ForEach(items) { item in
+					
 					Text(item.title)
 						.font(.title.weight(.light))
 						.padding(.vertical, 2)
@@ -34,6 +38,7 @@ struct ContentView: View {
 						.strikethrough(item.isCompleted)
 						.italic(item.isCompleted)
 						.swipeActions {
+							
 							Button(role: .destructive) {
 								withAnimation {
 									modelContext.delete(item)
@@ -41,17 +46,23 @@ struct ContentView: View {
 							} label: {
 								Label("Delete", systemImage: "trash")
 							}
+							
 						}
 						.swipeActions(edge: .leading) {
+							
 							Button("Done", systemImage: item.isCompleted == false ? "checkmark.circle" : "x.circle") {
 								item.isCompleted.toggle()
 							}
 							.tint(item.isCompleted == false ? .green : .accentColor)
+							
 						}
+					
 				}
+				
 			}
 			.navigationTitle("Grocery List")
 			.toolbar {
+				
 				if items.isEmpty {
 					ToolbarItem(placement: .topBarTrailing) {
 						Button {
@@ -61,11 +72,51 @@ struct ContentView: View {
 						}
 					}
 				}
+				
 			}
 			.overlay {
+				
 				if items.isEmpty {
 					ContentUnavailableView("Empty Cart", systemImage: "cart.circle", description: Text("Add some items to the shopping list."))
 				}
+				
+			}
+			.safeAreaInset(edge: .bottom) {
+				
+				VStack(spacing: 12) {
+					
+					TextField("", text: $item)
+						.textFieldStyle(.plain)
+						.padding(12)
+						.background(.tertiary)
+						.cornerRadius(12)
+						.font(.title.weight(.light))
+						.focused($isFocused)
+					
+					Button {
+						
+						guard !item.isEmpty else {
+							return
+						}
+						let newItem = Item(title: item, isCompleted: false)
+						modelContext.insert(newItem)
+						item = ""
+						isFocused = false
+						
+					} label: {
+						
+						Text("Save")
+							.font(.title2.weight(.medium))
+							.frame(maxWidth: .infinity)
+						
+					}
+					.buttonStyle(.borderedProminent)
+					.buttonBorderShape(.roundedRectangle)
+					.controlSize(.extraLarge)
+				}
+				.padding()
+				.background(.bar)
+				
 			}
 		}
     }
